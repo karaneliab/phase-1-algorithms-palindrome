d520792090196a91807a43c0b40def019c3e2acc
karaneliab <eliabkaran7884@gmail.com>
2024-01-23 12:21:36 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
-  const reverseword = reverse(word);
-  return word === reverseword;
+  // const reverseword = reverse(word);
+  // return word === reverseword;
+ 
+const cleanedWord = word.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+const wordArray =Array.from(cleanedWord)
+let newArray = [];
+for(let i= wordArray.length - 1; i >= 0; i--){
+  newArray.push(wordArray[i])
+}
+const reversedString = newArray.join('')
+if(reversedString === cleanedWord){
+  // if they match the word is palindrome
+  return true;
+}else {
+  // if they dont match,the word is not palindrome
+  return false;
 }
 
+}
+// Compare the original and reversed strings
+// return cleanedWord === cleanedWord.split('').reverse().join('');
 /* 
   Add your pseudocode here
 */
